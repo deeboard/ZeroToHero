f2b18855d287836b7affd7adb4af93040895ceba
deeboard <kingsleyugochukwu@yahoo.com>
2014-06-08 10:24:57 -0500

    Header
    Demo we created in Bootstrap Meeting
    (http://www.meetup.com/Programmers-crew/events/185230292/)

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Helpout ProgrammersCrew Tranning</title>
+
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	</head>
+	<body>
+		<div id="container">
+			<nav class="navbar navbar-default">
+				<div class="nav collapse navbar-collapse" id="top-brand">
+					<div class="container-fluid">
+						<ul class="navbar-nav nav navbar-left">
+							<a href="#" class="navbar-brand">Helpout</a>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<a href="#" class="navbar-brand">+pedro</a>
+						</ul>
+					</div>
+				</div>
+				<hr class="divider"/>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="nav collapse navbar-collapse" id="bottom">
+					<div class="container-fluid">
+						<ul class="navbar-nav nav navbar-left">
+							<li class="dropdown">
+								<a data-toggle="dropdown" href="#"> <span class="glyphicon glyphicon-home "></span> Browse Helpouts <span class="glyphicon glyphicon-chevron-down"></span> </a>
+
+								<ul class="dropdown-menu">
+									<li role="presentation" class="dropdown-header">
+										Helpout Selection Box
+									</li>
+									<li>
+										<a href="">Home</a>
+									</li>
+
+									<li>
+										<a href="">Art and Music</a>
+									</li>
+									<li>
+										<a href="">Computers And Eletronic</a>
+									</li>
+									<li>
+										<a href="">Cooking</a>
+									</li>
+									<li role="presentation" class="disabled">
+										<a role="menuitem" tabindex="-1" href="#">Education And Carrers</a>
+									</li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" data-toggle="dropdown"> <span class="glyphicon glyphicon-user"></span> Your Helpouts <span class="glyphicon glyphicon-chevron-down"></span></a>
+
+								<ul class="dropdown-menu">
+									<li>
+										<a  href="">Upcoming</a>
+									</li>
+									<li>
+										<a href="">Past</a>
+									</li>
+									<li>
+										<a href="">Referals</a>
+									</li>
+
+								</ul>
+							</li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<a href="#">About Helpout</a>
+							</li>
+							<li>
+								<a href="#"><span class="btn btn-primary btn-sm btn-success">Give your helpout</span></a>
+							</li>
+							<li>
+								<a href="#" data-toggle="dropdown"> <span class="glyphicon glyphicon-question-sign "></span></a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-cog"></span></a>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+			</nav>
+		</div>
+
+		<script src="js/jQuery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
